48cb1a835d327a13b6a8876b74fe4354eab4acf5
NguyenHung <nguyenhung.220102@gmail.com>
2024-03-24 10:23:09 +0700

    feat(master) remove config

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -1,11 +1,7 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
-import { config } from 'dotenv';
 
 import { Injectable } from '@nestjs/common';
-
-config();
-
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
